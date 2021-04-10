e28b7ce005e78aba1ceee74219f9de512b6f48bf
Klod <c.buisson@it-student.fr>
2021-04-10 20:39:20 +0200

    fire

--- js/script.js
+++ js/script.js
@@ -1,82 +1,103 @@
 window.addEventListener('scroll', () => {
     const header = document.getElementById('header');
     const rec = header.getBoundingClientRect();
-    console.log(rec);
+    // console.log(rec);
     const position = rec.bottom / window.innerHeight * 100;
-    console.log(document.getElementsByClassName('slice'));
+    // console.log(document.getElementsByClassName('slice'));
     document.querySelectorAll('.slice').forEach((div) => {
         div.style.height = `${position / 2}%`;
         if (position <= 1) {
             div.style.height = '0px';
         }
-        console.log(`${position}%`);
+        // console.log(`${position}%`);
     })
-    
+// ############### parallax earth ############### 
     let box = document.querySelector('.earth');
-    // let height = box.clientHeight;
-    // console.log(height);
-    
-    // window.addEventListener('scroll', el =>{
-        let scrolled = box.getBoundingClientRect();
-        let scrollPoss = scrolled.top *1.5;
+    let scrolled = box.getBoundingClientRect();
+    let scrollPoss = scrolled.top *1.5;
         
         const background = document.querySelectorAll(".island12, .island13");
-        // console.log(background);
         background.forEach((img) => {
-
             img.style.top = (scrollPoss) + "px";
         })
-
         const background2 = document.querySelectorAll(".island1, .island2");
         background2.forEach((img) => {
-
             img.style.top = (scrollPoss*2) + "px";
         })
-
         const background3 = document.querySelectorAll(".island3, .island7");
         background3.forEach((img) => {
-
             img.style.top = (scrollPoss*2.5) + "px";
         })
         const background4 = document.querySelectorAll(".island4, .island8");
         background4.forEach((img) => {
-
             img.style.top = (scrollPoss*3) + "px";
         })
         const background5 = document.querySelectorAll(".island5, .island9, .island10");
         background5.forEach((img) => {
-
             img.style.top = (scrollPoss*3.5) + "px";
         })
         const background6 = document.querySelectorAll(".island6, .island11");
         background6.forEach((img) => {
-
             img.style.top = (scrollPoss*4) + "px";
         })
         const cloud = document.querySelector(".clouds");
-        cloud.style.top = (scrollPoss*0.5) + "px";
-        
-        // console.log(scrollPoss);
-
-        
-
+        cloud.style.top = (scrollPoss*0.5) + "px";    
+    
+    
+    let earth = scrolled.top;
+    
+    if(earth < -150){
+        console.log(earth);
+    document.querySelectorAll('#flower1, #flower2, #flower3, #flower4').forEach((img) =>{
+        img.classList.remove('swing');
+    })} 
+    else if(earth < -41.85){
+        console.log(earth);
+    document.querySelectorAll('#flower1, #flower2, #flower3, #flower4').forEach((img) =>{
+        img.classList.add('swing');
+    })
+    }else if(earth > -41.85 ){
+        console.log(earth);
+    document.querySelectorAll('#flower1, #flower2, #flower3, #flower4').forEach((img) =>{
+        img.classList.remove('swing');
+    })}
 }, false);
-// })
 
-$(document).ready(function()
-{
+// ############### flower animation ############### 
+
+$(document).ready(function(){
     $("#flower1, #flower2, #flower3, #flower4").hover(
         function()
         {
-            $(this).attr("src", "public/img/flowerOnce.gif");
+            $(this).attr("src", "public/img/earth/flowerOnce.gif");
         },
         function()
         {
-            $(this).attr("src", "public/img/flowerFixed.gif");
+            $(this).attr("src", "public/img/earth/flowerFixed.gif");
         });
-        // $("#flower1").tooltip({
-        //     content: "Awesome title!"
-        //   });
-        //   $( "#flower1" ).tooltip( "option", "content", "Awesome title!" );
-       
+});
+
+// let box = document.querySelector('.fire');
+// let scrolled = box.getBoundingClientRect();
+
+// ############### fire carousel ############### 
+var carousel = document.querySelector('.carousel');
+var cellCount = 6;
+var selectedIndex = 0;
+
+function rotateCarousel() {
+  var angle = selectedIndex / cellCount * -360;
+  carousel.style.transform = 'translateZ(-288px) rotateY(' + angle + 'deg)';
+}
+
+var prevButton = document.querySelector('.previous-button');
+prevButton.addEventListener( 'click', function() {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+var nextButton = document.querySelector('.next-button');
+nextButton.addEventListener( 'click', function() {
+  selectedIndex++;
+  rotateCarousel();
 });
